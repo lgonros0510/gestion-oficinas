c90751ff1a022fd39665dee0b6f379d06406054b
lgonros0510 <151656381+lgonros0510@users.noreply.github.com>
2025-03-06 17:36:46 +0100

    Tercera actualización en create.blade.php
    Un apequeña actualización para que se vean los errores que haya al introducir elementos en este formulario.

--- resources/views/empleados/create.blade.php
+++ resources/views/empleados/create.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <h1 class="mb-4">Crear un empleado para "{{ $oficina->nombre }}"</h1>
     <form action="{{ route('oficinas.empleados.store', $oficina) }}" method="POST">
         @csrf
